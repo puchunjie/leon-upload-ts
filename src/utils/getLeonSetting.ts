e17c19db2b278711c5bd22a433fc2d8572805b98
jeremy.pu <puchunjie@live.com>
2021-04-08 11:58:50 +0800

    删除 上传域名配置

--- src/utils/getLeonSetting.ts
+++ src/utils/getLeonSetting.ts
@@ -3,7 +3,7 @@ const leonConfig = vscode.workspace.getConfiguration("leon_upload_image");
 
 export default {
   bucketName: leonConfig.bucket,
-  apiUrl: leonConfig.domain,
+  apiUrl: leonConfig.domain || 'http://leonidapi.17usoft.com/libraapi2/leonid/v2/static/object',
   userToken: leonConfig.userToken,
   assetKey: leonConfig.accessKey,
   folder: leonConfig.folder || "/",
